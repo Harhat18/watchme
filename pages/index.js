a2ebfcadb0e9c98dab7204b80ccd85eb4a1c1463
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-01-01 01:10:45 +0300

    add API

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import Hero from "../components/Hero";
+import { server } from "../config";
 
 export default function Home({ movies }) {
   return (
@@ -6,4 +8,15 @@ export default function Home({ movies }) {
       <Hero />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await axios(
+    `${server}/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+  );
+  const movies = res.data;
+
+  return {
+    props: { movies },
+  };
 }
