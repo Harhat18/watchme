7d97a12c7b5e462aeadf0736d8e2c63d417a47e3
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-01-01 01:32:00 +0300

    add moviecard

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import Hero from "../components/Hero";
+import PopularMovie from "../components/PopularMovie";
 import { server } from "../config";
 
 export default function Home({ movies }) {
   return (
     <div className="bg-gray-700">
       <Hero />
+      <PopularMovie movies={movies.results} />
     </div>
   );
 }
