a4b02208f410ceba221e49a5090fe5315fa4b388
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-12-31 07:05:41 +0300

    add Navbar components

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 const contact = () => {
   return (
-    <div className="text-center mt-20 ">
-      <h1 className="text-4xl font-bold">Contact Us</h1>
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">Contact US</h1>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Hero from "../components/Hero";
-export default function Home() {
+
+export default function Home({ movies }) {
   return (
-    <div>
+    <div className="bg-gray-700">
       <Hero />
     </div>
   );
